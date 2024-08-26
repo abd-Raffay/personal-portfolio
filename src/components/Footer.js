18e7837f742c354cd2760666166c6632b597ea11
abd-Raffay <rraffay38@gmail.com>
2024-08-26 13:38:49 +0500

    fixed theme and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,9 +16,10 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={linkedinLogo} alt="Icon" /></a>
-              <a href="#"><img src={githubLogo} alt="Icon" /></a>
-              <a href="#"><img src={instagramLogo} alt="Icon" /></a>
+            <a href="https://www.linkedin.com/in/abd-raffay/" target="_blank" rel="noopener noreferrer"><img src={linkedinLogo} alt="" /></a>
+
+<a href="https://github.com/abd-Raffay" target="_blank" rel="noopener noreferrer"><img src={githubLogo} alt="" /></a>
+<a href="https://www.instagram.com/abd_raffay?igsh=MWFvejB5eG5kbTJxMg%3D%3D&utm_source=qr" target="_blank" rel="noopener noreferrer"><img src={instagramLogo} alt="" /></a>
             </div>
             <p>Copyright 2024. All Rights Reserved</p>
           </Col>
